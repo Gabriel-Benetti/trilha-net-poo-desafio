6f50e0bd78f8dd3875efcabd9c6a3b95a228d77e
Gabriel <gabrielf.ni10@gmail.com>
2024-04-14 12:59:31 -0300

    colocado os metodos de ligação

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ Nokia n154 = new Nokia("44 9554 - 6464", "65146FD6DDSC6465", "Nokia Tijolão", 2
 
 Console.WriteLine($"Modelo: {i11.Modelo} IMEI: {i11.IMEI} Numero: {i11.Numero} Memoria: {i11.Memoria}");
 i11.InstalarAplicativo("ZAP ZAP 2");
+i11.Ligar();
+i11.ReceberLigacao();
 
 Console.WriteLine("\n\n");
 
 Console.WriteLine($"Modelo: {n154.Modelo} IMEI: {n154.IMEI} Numero: {n154.Numero} Memoria: {n154.Memoria}");
 n154.InstalarAplicativo("Raio x da natta.com");
+n154.Ligar();
+n154.ReceberLigacao();
 
 
 
